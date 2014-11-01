12aefa7ec239a465ac9f0442aff7e3d1a81ef131
GeckoOnRadio <konrad.wolf84@gmail.com>
2014-11-01 13:08:04 +0100

    Add CloudFace again

---
+++ Limbus.CloudFace/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AppHarbor;
+using System.Security.Authentication;
+using System.IO;
+using System.Reflection;
+
+namespace Limbus.CloudFace
+{
+	public class Program
+	{
+		public static void Main(string[] arguments)
+		{
+			CheckUsage(arguments);
+
+			string clientId = arguments[0];
+			string clientSecret = arguments[1];
+
+			Console.WriteLine("Please authorize this application in the browser window that has just opened");
+			var authInfo = GetAuthorization(clientId, clientSecret);
+
+			Console.WriteLine("Authorization successful");
+			var client = new AppHarborClient(authInfo);
+
+			Console.WriteLine("Applications:");
+			foreach (var application in client.GetApplications())
+			{
+				Console.WriteLine(" - {0}", application.Name);
+			}
+		}
+
+		private static void CheckUsage(string[] arguments)
+		{
+			if (arguments.Length != 2)
+			{
+				Console.WriteLine("Usage: {0} [clientId] [clientSecret]", ExecutableName);
+				Environment.Exit(1);
+			}
+		}
+
+		private static AuthInfo GetAuthorization(string clientId, string clientSecret)
+		{
+			try
+			{
+				return AppHarborClient.AskForAuthorization(clientId, clientSecret, TimeSpan.FromMinutes(1));
+			}
+			catch (AuthenticationException)
+			{
+				Console.WriteLine("Failed to get authorization");
+				Environment.Exit(-1);
+				throw;
+			}
+			catch (TimeoutException)
+			{
+				Console.WriteLine("Timeout, you have to be faster than that");
+				Environment.Exit(-1);
+				throw;
+			}
+		}
+
+		private static string ExecutableName
+		{
+			get
+			{
+				return Path.GetFileName(Assembly.GetEntryAssembly().Location);
+			}
+		}
+	}
+}
